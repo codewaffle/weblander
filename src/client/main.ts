1516d7f5b4d09e605de07f6de3b8c7304f07d587
Patrick Griffus <patrick@codewaffle.com>
2017-06-17 17:24:10 -0400

    move gravity calculation from client to World

--- src/client/main.ts
+++ src/client/main.ts
@@ -31,27 +31,12 @@ export class GameClient extends PIXI.Application implements IOnRender, IOnFixedU
             gravity: [0, 0],
         });
 
-
-        this.world.applyDamping = false;
-        this.world.applyGravity = false;
-        this.world.defaultContactMaterial.friction = 0.8;
-
-        this.planets = [];
-
-        let t : number[][] = [];
-
-        for(let r = 0; r < 2*Math.PI;r+=Math.PI/64) {
-            t.push([5000 * Math.cos(r), 5000 * Math.sin(r)]);
-        }
-
-        let p = new Planetoid(t);
-        let pd = new PlanetoidDisplay(p);
+        let pd = new PlanetoidDisplay(this.world.spawnDevPlanet());
 
         this.stage.addChild(pd);
 
         let ship = this.b = new Ship();
         this.world.addBody(ship);
-        this.world.addBody(p);
         
         let s = new ShipDisplay(ship);
         this.stage.addChild(s);
@@ -88,13 +73,8 @@ export class GameClient extends PIXI.Application implements IOnRender, IOnFixedU
             this.b.angle += dt * 4;
         }
 
-        let _mass = 1000000000;
-
-        let mag = _mass/vec2.squaredLength(this.b.position);
-        let grav = [0, 0];
-
-        vec2.normalize(grav, this.b.position);
-        vec2.scale(grav, grav, -mag * dt);
+        let grav = this.world.calculateGravityAtPoint(this.b.position);
+        vec2.scale(grav, grav, dt);
 
         vec2.add(this.b.velocity, this.b.velocity, grav);
     }
--- src/common/World.ts
+++ src/common/World.ts
@@ -1,7 +1,42 @@
 import * as p2 from 'p2'
+import {Planetoid, Ship} from '.'
 
 export class World extends p2.World {
-    public calculateGravityAtPoint(point : number[]) : number [] {
-        return [0, 0];
+    private _planets : Planetoid[];
+    private _ships : Ship[];
+
+    constructor(options : any) {
+        super(options);
+
+        this.applyDamping = false;
+        this.applyGravity = false;
+        this.defaultContactMaterial.friction = 0.8;
+
+        this._planets = [];
+        this._ships = [];
+    }
+
+    public calculateGravityAtPoint(point : number[]) : number[] {
+        let _mass = 1000000000;
+        let mag = _mass/p2.vec2.squaredLength(point);
+        let grav = [0, 0];
+
+        p2.vec2.normalize(grav, point);
+        p2.vec2.scale(grav, grav, -mag);
+
+        return grav;
+    }
+
+    public spawnDevPlanet() : Planetoid {
+        let t : number[][] = [];
+
+        for(let r = 0; r < 2*Math.PI;r+=Math.PI/64) {
+            t.push([5000 * Math.cos(r), 5000 * Math.sin(r)]);
+        }
+
+        let rv = new Planetoid(t);
+        this.addBody(rv);
+
+        return rv;
     }
 }
